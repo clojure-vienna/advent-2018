3e1c01c13e2926a9b8260513ed628ec969ae13b1
Herwig Hochleitner <herwig@bendlas.net>
2018-12-01 17:19:14 +0100

    [bendlas] day 1

---
+++ src/clojure_vienna/bendlas/day_1.clj
@@ -0,0 +1,25 @@
+(ns clojure-vienna.bendlas.day-1
+  (:require [clojure.java.io :as io]))
+
+(def inputs
+  (into [] (map #(Long/parseLong %))
+        (line-seq (io/reader (io/resource "clojure-vienna/bendlas/day-1")))))
+
+(def result-1
+  (reduce + 0 inputs))
+
+(println "Result 1:" result-1)
+
+(def result-2
+  (->> inputs
+       cycle
+       (reductions + 0)
+       (map-indexed vector)
+       (reduce (fn [memory [index value]]
+                 (if (contains? memory value)
+                   (reduced [index value])
+                   (conj memory value)))
+               #{})))
+
+(println "Result 2:" (second result-2)
+         "found after" (first result-2) "iterations")
