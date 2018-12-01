761bdebb465d0d8f472ef66a72d9b3fcc12dcb38
Herwig Hochleitner <herwig@bendlas.net>
2018-12-01 17:19:05 +0100

    init checkin, setup project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.meetup.clojure-vienna/advent-2018 "0.1.0-SNAPSHOT"
+  :description "clojure-vienna's advent of code implementations for 2018"
+  :url "https://adventofcode.com/2018/"
+  :license {:name "CC0"
+            :url "https://creativecommons.org/publicdomain/zero/1.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0-RC2"]])
