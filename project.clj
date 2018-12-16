414cb7aaa78c0054350feddfe85007c83c398c88
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-12-16 19:50:24 +0100

    [pebrc] day1

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "CC0"
             :url "https://creativecommons.org/publicdomain/zero/1.0/"}
   :main clojure-vienna.advent-2018
-  :dependencies [[org.clojure/clojure "1.10.0-RC2"]]
+  :dependencies [[org.clojure/clojure "1.10.0-RC2"]
+                 [org.clojure/tools.logging "0.4.1"]]
   :plugins [[lein-autoreload "0.1.1"]])
